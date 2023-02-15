c1d0250a081deee7f21c939021998ab2108d93ef
igorantunes04 <125463464+igorantunes04@users.noreply.github.com>
2023-02-15 09:40:56 -0300

    Add files via upload

---
+++ relogioDigital/index.js
@@ -0,0 +1,22 @@
+const horas =  document.getElementById("horas");
+const minutos =  document.getElementById("minutos");
+const segundos =  document.getElementById("segundos");
+
+const relogio = setInterval(function time(){
+    let tempo = new Date();
+    let hr = tempo.getHours();
+    let min = tempo.getMinutes();
+    let seg = tempo.getSeconds();
+    if(hr <10){
+        hr = "0" + hr;
+    }
+    if(min <10){
+        min = "0" + min;
+    }
+    if(seg <10){
+        seg = "0" + seg;
+    }
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+})
